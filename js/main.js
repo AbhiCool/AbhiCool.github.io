8747382c69c35138bbdbd87afed7c82b776a90f9
Abhijit Naik Govekar <abhijitnaikgovekar@gmail.com>
2020-01-10 23:37:15 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $('.nav li').click(function(event) {
+    event.preventDefault();
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+
+    var $section = $($(this).find('a').attr('href')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top - 50
+    }, 800);
+  });
+
+  // When the user clicks on scrollToTopBtn button, scroll to the top of the document
+  $('.scrollToTopBtn').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500);
+  });
+
+  $('.feedbackForm').on("submit", function(e) {
+    e.preventDefault();
+  });
+
+  $('.feedbackForm').on("click", '.clearBtn', function(e) {
+    e.preventDefault();
+
+    $('.feedbackForm')[0].reset();
+  });
+});// End of document.ready
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+$(window).on('scroll', function() {
+  scrollFunction();
+  function scrollFunction() {
+    //Get the button:
+    var scrollToTopBtn = $(".scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.show();
+      return;
+    }
+    scrollToTopBtn.hide();
+
+  }
+}); // End of window scroll event
